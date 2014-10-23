06c09a455c1d2836165eb4f4b5a79a20d70d2ed8
Nicholas Nelson <nelsonni@onid.oregonstate.edu>
2014-10-22 23:27:10 -0700

    switched from srand to rdrand for random number generation, added infinite execution loop for steps: think (1), get_forks (2), eat (3), and put_forks (4)

--- assignment2/philosophers.c
+++ assignment2/philosophers.c
@@ -1,16 +1,17 @@
 #define NUM_THREADS 5
+#define asm __asm__ __volatile__
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <time.h>	// required for srand
-#include <stdlib.h>	// required for srand
 
 void *table(void *philosopher);
 void think(void *philosopher);
 void get_forks(void *philosopher);
 void eat(void *philosopher);
+void put_forks(void *philosopher);
 
 sem_t mutex;
 
@@ -19,14 +20,13 @@ int main()
 	pthread_t threads[NUM_THREADS];
 	sem_init(&mutex, 0, 2);
 	char *philosophers[] = {"Aristotle", "Plato", "Rene Descartes", "Confucius", "John Locke"};
-	int num_philosophers = 5;
 
 	int rc;
 	long t;
-	for (t = 0; t < num_philosophers; t++) {
+	for (t = 0; t < NUM_THREADS; t++) {
 		rc = pthread_create(&threads[t], NULL, table, (void *)philosophers[t]);
 		if (rc) {
-			printf("ERROR; return code from pthread_create() is %d\n", rc);
+			printf("ERROR: return code from pthread_create() is %d\n", rc);
 			exit(-1);
 		}
 	}
@@ -39,31 +39,33 @@ void *table(void *philosopher)
 	char *ph = (char *) philosopher;
 	printf("%s has arrived at the table\n", ph);
 
-	think((void *)ph);
-	get_forks((void *)ph);
-
+	while (true) {
+		think((void *)ph);
+		get_forks((void *)ph);
+		eat((void *)ph);
+		put_forks((void *)ph);
+	}
+	
 	pthread_exit(NULL);
-}
-
-void get_forks(void *philosopher)
-{
-	char *ph = (char *) philosopher;
-
-	sem_wait(&mutex);
-	printf("%s got two forks\n", ph);
-	eat((void *)ph);
-	sem_post(&mutex);
-	printf("%s returned two forks\n", ph);
 }
 
 void think(void *philosopher)
 {
 	char *ph = (char *) philosopher;
-	srand(time(NULL));
+	
+	int think_time = 0;
+	asm("rdrand %0" : "=r" (think_time));
+	think_time = abs(think_time) % 20 + 1;
 
-	int think_time = rand() % 20 + 1;
 	printf("%s is thinking for %d seconds\n", ph, think_time);
 	sleep(think_time);
+}
+
+void get_forks(void *philosopher)
+{
+	char *ph = (char *) philosopher;
+	sem_wait(&mutex);
+	printf("%s got two forks\n", ph);
 }
 
 void eat(void *philosopher)
@@ -71,7 +73,17 @@ void eat(void *philosopher)
 	char *ph = (char *) philosopher;
 	srand(time(NULL));
 
-	int eat_time = rand() % 9 + 2;
+	int eat_time = 0;
+	asm("rdrand %0" : "=r" (eat_time));
+	eat_time = abs(eat_time) % 9 + 2;
+
 	printf("%s is eating for %d seconds\n", ph, eat_time);
 	sleep(eat_time);
+}
+
+void put_forks(void *philosopher)
+{
+	char *ph = (char *) philosopher;
+	sem_post(&mutex);
+	printf("%s returned two forks\n", ph);
 }
